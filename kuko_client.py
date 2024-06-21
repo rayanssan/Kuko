3165175bd21c7c7876a9084044d9fb1b5f4b9608
rayanssan <126510004+rayanssan@users.noreply.github.com>
2024-06-21 16:25:09 +0100

    Update kuko_client.py

--- kuko_client.py
+++ kuko_client.py
@@ -1,9 +1,3 @@
-"""
-Aplicações Distribuídas - Projeto 3 - kuko_client.py
-Grupo: 01
-Números de aluno: 60282
-"""
-
 import sys, json
 from net_client import *
 from kazoo.client import KazooClient
